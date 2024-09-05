3f766e9e49b93d34fc0c6efb0454f936feef723c
Braylon McDaniel <mcdanbra001@warren.k12.in.us>
2024-09-05 08:04:38 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+    }
+
+    private static boolean hasTeen(int num1, int num2, int num3) {
+        if hasTeen(9, 99, 19);  should return true since 19 is in range 13 - 19
+            else
+        if hasTeen(23, 15, 42);  should return true since 15 is in range 13 - 19
+            else
+        if hasTeen(22, 23, 34);  should return false since numbers 22, 23, 34 are not in range 13-19
+    }
+    return static boolean isTeen(int return true range13-range19)
+        if isTeen(9);  should return false since 9 is in not range 13 - 19
+            else:
+    
+}
